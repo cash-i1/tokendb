3c8f2abe2800838dddad6e7bc957665b270c1458
c <cashignitro@gmail.com>
2024-10-20 12:25:46 +1100

    added tiny_http and maud boilerplate from another project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::path::Path;
+use tiny_http::Header;
+use tiny_http::Response;
+use tiny_http::Server;
+
 fn main() {
-    println!("Hello, world!");
+    let server = Server::http("0.0.0.0:2333").unwrap();
+
+    for request in server.incoming_requests() {
+        let path = request.url();
+        let segments: Vec<&str> = path.trim_matches('/').split('/').collect();
+
+        let response = match segments[..] {
+            ["assets", path] => {
+                let file_path = Path::new("./assets/").join(path);
+                let string = std::fs::read_to_string(file_path).unwrap();
+
+                Response::from_string(string).with_header(Header {
+                    field: "Content-Type".parse().unwrap(),
+                    value: "text/plain".parse().unwrap(),
+                })
+            }
+            _ => Response::from_string("".to_string()),
+        };
+
+        request.respond(response).unwrap();
+    }
 }
